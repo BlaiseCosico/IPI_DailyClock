3e3bfa2ca24ec959c1441150f747ab1f18c3c751
BlaiseCosico <Blaisecosico101@gmail.com>
2019-10-18 15:35:56 +0800

    added timeout function

--- timesheet/users/routes.py
+++ timesheet/users/routes.py
@@ -8,9 +8,23 @@ from timesheet.models import User, Daily
 from timesheet.users.forms import LoginForm
 
 users = Blueprint('users', __name__)
+todays_datetime = datetime(datetime.today().year, datetime.today().month, datetime.today().day)
 
 @users.route('/login', methods=['GET','POST'])
 def login():
+
+    def time_in(): #refactor
+
+        check_user = Daily.query.filter_by(user_id=current_user.username).filter_by(date_today=todays_datetime).first()    #check if name and date today exists day.date_today.strftime("%Y/%m/%d
+        if check_user is None:
+            day = Daily(time_in=datetime.now().strftime("%H:%M"), user_id=current_user.username) #remove seconds
+            db.session.add(day)
+            db.session.commit()
+
+            return day
+
+        return None
+
     if current_user.is_authenticated:
         return redirect(url_for('main.home'))
     form = LoginForm()
@@ -31,16 +45,22 @@ def logout():
     logout_user()
     return redirect(url_for('main.home'))
 
+@users.route('/time_out')
+@login_required
+def time_out():
+    #get user with current_user.username and date today
+    user = Daily.query.filter_by(user_id=current_user.username).filter_by(date_today=todays_datetime).first()
+    user.time_out = datetime.now().strftime("%H:%M")
+    db.session.commit()
+    flash('You have timed out!', 'success')
+    daily = Daily.query.order_by(Daily.time_in.desc())
+    #user is loggged in
+    #press timeout button
+    #get current_user, relationship with daily
+    #update timeout
+    #pass updated timeout info to page again
 
-def time_in(): #refactor
-    todays_datetime = datetime(datetime.today().year, datetime.today().month, datetime.today().day)
+    return render_template('posts.html', title='Timed Out',
+                           daily=daily)
 
-    check_user = Daily.query.filter_by(user_id=current_user.name).filter_by(date_today=todays_datetime).first()    #check if name and date today exists day.date_today.strftime("%Y/%m/%d
-    if check_user is None:
-        day = Daily(time_in=datetime.now().strftime("%H:%M"), user_id=current_user.name) #remove seconds
-        db.session.add(day)
-        db.session.commit()
 
-        return day
-
-    return None
