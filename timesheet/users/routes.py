a82bfb942a03aa9c83f3201ce212d371df5d15ab
BlaiseCosico <Blaisecosico101@gmail.com>
2019-10-18 12:37:37 +0800

    refactored code in time_in

--- timesheet/users/routes.py
+++ timesheet/users/routes.py
@@ -1,8 +1,9 @@
+import sys
 from flask import (render_template, url_for, flash,
                    redirect, request, abort, Blueprint)
 from flask_login import login_user, current_user, logout_user, login_required
 from timesheet import db
-from datetime import datetime
+from datetime import datetime, date
 from timesheet.models import User, Daily
 from timesheet.users.forms import LoginForm
 
@@ -16,24 +17,30 @@ def login():
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user and user.password == form.password.data:
-            login_user(user) #remember me?
-            day = Daily(time_in=datetime.now().strftime("%H:%M:%S"), user_id=current_user.name)
-            db.session.add(day)
-            db.session.commit()
-            #Get userID
-            #check Daily if already logged in today (?)
-            #if yes, continue;
-            #else, Add record to daily
+            login_user(user)
+            time_in()
 
-            daily = Daily.query.all()
-            # return redirect(url_for('main.posts_'))
             return redirect(url_for('main.home'))
         else:
             flash('Login Unsuccessful. Please check email and password', 'danger')
     return render_template('login.html', title='login', form=form)
     
-    
+
 @users.route('/logout')
 def logout():
     logout_user()
     return redirect(url_for('main.home'))
+
+
+def time_in(): #refactor
+    todays_datetime = datetime(datetime.today().year, datetime.today().month, datetime.today().day)
+
+    check_user = Daily.query.filter_by(user_id=current_user.name).filter_by(date_today=todays_datetime).first()    #check if name and date today exists day.date_today.strftime("%Y/%m/%d
+    if check_user is None:
+        day = Daily(time_in=datetime.now().strftime("%H:%M"), user_id=current_user.name) #remove seconds
+        db.session.add(day)
+        db.session.commit()
+
+        return day
+
+    return None
